203014c242f52e30756b3b339a19c5a9f1100aa7
pramebt <bdforwk@gmail.com>
2025-05-06 17:15:42 +0700

    in register receive the value (username,email,password) then create hash for passwod
    in login  receive the value (email, password) compare the hash
    
    if the password is correct will create jwt (generateToken)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 const{findUserByEmail,createUser} = require('../models/user')
 const db = require("../config/db")
 const bcrypt = require("bcrypt");
+const generateToken = require('../utils/generateToken')
+
 exports.register = async(req,res) => {
     try{
         const { username, email, password } = req.body;
@@ -25,10 +27,27 @@ exports.register = async(req,res) => {
 
 exports.login = async(req,res) => {
     try{
-        res.status(200).json({
-            success:true,
-            message:"Login successfully"
-        })
+         const {email,password} = req.body;
+         const username = await findUserByEmail(email);
+         if(!username){
+            return res.status(400).send("Username does not exist")
+         }
+
+         const validPassword = await bcrypt.compare(password, username.password)
+         if(!validPassword){
+            return res.status(400).send("Invalid password")
+         }
+
+         const token = generateToken(username.id);
+            res.json({
+            token,
+            username: {
+                id: username.id,
+                username: username.username,
+                email: username.email,
+               
+            }
+        });
     }catch(err){
         console.log(err,{message:err})
     }
