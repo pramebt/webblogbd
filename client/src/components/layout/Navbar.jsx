dab9a4c817b468e22857028770e7ea5d3dc777e7
pramebt <bdforwk@gmail.com>
2025-05-07 18:39:04 +0700

    navbar complete

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,83 @@
+import React,{useState} from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="border border-gray-400 bg-white flex justify-between items-center fixed  px-5 md:px-20  w-full h-20 z-10">
+      <a href="/">bdblog</a>
+      <ul className="hidden md:flex space-x-10">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/post">Post</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+
+      <div
+        className={`absolute top-16 right-0 bg-black text-white shadow-lg rounded-lg p-5 md:hidden transition-transform duration-300 ease-in-out 
+                ${isOpen ? "translate-x-0" : "translate-x-full"} z-40`}
+      >
+        <ul className="space-y-4">
+          <li>
+            <a href="/about-us">About us</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+          <li>
+            <a href="/use-cases">Use Cases</a>
+          </li>
+          <li>
+            <a href="/pricing">Pricing</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Hamburger Button */}
+      <button
+        onClick={handleClick}
+        className="md:hidden flex flex-col justify-center items-center "
+      >
+        <span
+          className={`bg-black block transition-all duration-300 ease-out 
+                        h-0.5 w-5 rounded-sm ${
+                          isOpen
+                            ? "rotate-45 translate-y-1"
+                            : "-translate-y-0.5"
+                        }`}
+        ></span>
+        <span
+          className={`bg-black block transition-all duration-300 ease-out 
+                        h-0.5 w-5 rounded-sm my-0.5 ${
+                          isOpen ? "opacity-0" : "opacity-100"
+                        }`}
+        ></span>
+        <span
+          className={`bg-black block transition-all duration-300 ease-out 
+                        h-0.5 w-5 rounded-sm ${
+                          isOpen
+                            ? "-rotate-45 -translate-y-1"
+                            : "translate-y-0.5"
+                        }`}
+        ></span>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='pt-20 px-20'>
+        <div className='flex justify-center md:pt-10 pt-5'>
+            <img src="/assets/images/background2.svg" alt="background2" />
+        </div>
+        <div className="text-center mt-5">
+            <h1>Welcome to bdblog</h1>
+            <p>Your go-to platform for insightful posts.</p>
+        </div>
+    </div>
   )
 }
 
