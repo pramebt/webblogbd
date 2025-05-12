3e4062e4448b14f81b452b2a6e3639d998af0dbe
pramebt <bdforwk@gmail.com>
2025-05-12 10:46:44 +0700

    upload loading

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,52 +26,58 @@ const Home = () => {
   return (
     <div className="px-20 ">
       <div className="flex flex-col items-center justify-center gap-5 pt-5">
-        <h1 className="text-2xl md:text-[52px] ">
-          Blogs Of Developer
-        </h1>
-        <img 
-        src="/assets/images/undraw_programming.svg" 
-        alt="background2" 
-        className="block md:hidden w-100"/>
-        <img 
-        src="/assets/images/undraw_quiet-street.svg" 
-        alt="background2" 
-        className="hidden md:block w-150"/>
+        <h1 className="text-2xl md:text-[52px] ">Blogs Of Developer</h1>
+        <img
+          src="/assets/images/undraw_programming.svg"
+          alt="background2"
+          className="block md:hidden w-100"
+        />
+        <img
+          src="/assets/images/undraw_quiet-street.svg"
+          alt="background2"
+          className="hidden md:block w-150"
+        />
       </div>
 
       <div className="font-jetbrains text-center mt-5 ">
         <h1>Learn new techniques and best practices for popular tools.</h1>
       </div>
 
-
-        <div className="flex justify-center mt-6 mb-6">
-          <button
-            onClick={() => navigate('/blogs')}
-            className="px-6 py-3 bg-[#eaaa3c] text-white rounded-lg cursor-pointer"
-          >
-            Read more blogs
-          </button>
-
-        </div>
-      
-      {/* grid blog preview */}
-      <div className="mt-5 px-2 md:px-10 max-w-5xl mx-auto">
-        <div className="grid w-full h-fit gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          {previewData.map((item, index) => (
-            <div
-              key={index}
-              className="group cursor-pointer "
-              onClick={() => navigate(`/blogs/${item.id}`)}
-            >
-              <Cardblogview item={item} />
-            </div>
-          ))}
-        </div>
+      <div className="flex justify-center mt-6 mb-6">
+        <button
+          onClick={() => navigate("/blogs")}
+          className="px-6 py-3 bg-[#eaaa3c] text-white rounded-lg cursor-pointer"
+        >
+          Read more blogs
+        </button>
       </div>
 
-      
-
-      
+      {/* grid blog preview */}
+      <div className="mt-5 px-2 md:px-10 max-w-5xl mx-auto">
+        {previewData.length === 0 ? (
+          // แสดง GIF โหลดเมื่อยังไม่มีข้อมูล
+          <div className="flex justify-center py-20">
+            <img
+              src="/assets/Rolling.gif"
+              alt="Loading..."
+              className=""
+            />
+          </div>
+        ) : (
+          // แสดงตารางเมื่่อมีข้อมูลแล้ว
+          <div className="grid w-full h-fit gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            {previewData.map((item, index) => (
+              <div
+                key={index}
+                className="group cursor-pointer"
+                onClick={() => navigate(`/blogs/${item.id}`)}
+              >
+                <Cardblogview item={item} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- client/src/pages/Nopage.jsx
+++ client/src/pages/Nopage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Nopage = () => {
   return (
-    <div>404 NoPage</div>
-  )
-}
+    <div className="mt-10 w-full flex justify-center ">
+      <div className="flex flex-col">
+        <img
+          src="/assets/images/undraw_quiet-street.svg"
+          alt="quietstreet"
+          className="w-130"
+        />
+        <h1 className="mt-5 text-center text-4xl ">Not Found Page</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Nopage
+export default Nopage;
